6bc913a3929c8871bfe106a5ca722bfd9471b5ec
Paulo <God33@jkl>
2022-11-17 00:29:17 -0300

    Subindo o projeto - configuração inicial

---
+++ src/main/java/one/digitalinnovation/spring/ProjetoSpringApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringApplication.class, args);
+	}
+
+}
